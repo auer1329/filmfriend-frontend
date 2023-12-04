7a1004d55c5167f9dd6c603a4a025f1d99290fe3
ZieglerAaron <s0588522@htw-berlin.de>
2023-12-04 21:49:36 +0100

    Fix the stock images
    Co-authored-by: TheoThmn <s0586704@htw-berlin.de>

--- src/components/MyStockList.vue
+++ src/components/MyStockList.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="row row-cols-1 row-cols-md-4 g-4">
     <div class="col" v-for="stock in stocks" :key="stock.id">
       <div class="card">
-        <img :src=stock.staticImageURL class="card-img-top" :alt="stock.name">
+        <img :src=stock.staticImageUrl class="card-img-top" :alt="stock.name">
         <div class="card-body">
           <h5 class="card-title">{{ stock.brand }} {{ stock.name }}</h5>
           <p class="card-text">{{ stock.description }}</p>
