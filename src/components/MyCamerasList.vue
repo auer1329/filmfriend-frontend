fda5153f5d364c8865924e43751bb85ac6f6612a
ZieglerAaron <s0588522@htw-berlin.de>
2023-12-15 22:24:06 +0100

    Insert new camera form next to cam-list
    Co-authored-by: TheoThmn <s0586704@htw-berlin.de>

--- src/components/MyCamerasList.vue
+++ src/components/MyCamerasList.vue
@@ -1,27 +1,43 @@
 <template>
   <div class="row row-cols-1 row-cols-md-3 g-4">
     <div  class="card mb-3">
+      <br>
       <h5 class="card-title">Neue Kamera</h5>
-      <!-- Button trigger modal -->
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-        Neue Kamera <br>
-        hinzufügen
-      </button>
-      <!-- Modal -->
-      <FriendlyModal title="Neue Kamera" :modalContent=NewCameraForm submitText="Kamera hinzufügen" />
-    </div>
-    <div class="col" v-for="camera in cameras" :key="camera.id">
 
+      <form>
+        <div>
+          <br>
+          <input type="text" class="form-control" v-model="nameField" placeholder="Kameraname" required>
+          <br>
+          <input type="text" class="form-control" v-model="brandField" placeholder="Marke" required>
+          <br>
+          <input type="text" class="form-control" v-model="modelField" placeholder="Modell" required>
+          <br>
+          <input type="text" class="form-control" v-model="staticImageUrlField" placeholder="Bild">
+          <br>
+          <select class="form-select" id="formatSelection" required>
+            <option selected>Format auswählen</option>
+            <option value="35">35mm</option>
+            <option value="120">120mm</option>
+          </select>
+          <br>
+        </div>
+      </form>
+      <button type="button" class="btn btn-primary" @click="save()">Kamera hinzufügen</button>
+      <br>
+    </div>
+
+    <div class="col" v-for="camera in cameras" :key="camera.id">
       <div class="card " style="max-width: 540px">
         <img :src=camera.staticImageUrl class="card-image-top rounded-start" :alt="camera.name" >
-          <div class="card-body">
-              <h5 class="card-title">{{ camera.name }}</h5>
-              <p class="card-text">
-                {{ camera.brand }} {{ camera.model }}  <br>
-                <div v-if="camera.formatThirtyFive">Format: 35mm</div>
-                <div v-if="camera.formatOneTwenty">Format: 120mm</div>
-              </p>
-          </div>
+        <div class="card-body">
+            <h5 class="card-title">{{ camera.name }}</h5>
+            <div class="card-text">
+              {{ camera.brand }} {{ camera.model }}  <br>
+              <div v-if="camera.formatThirtyFive">Format: 35mm</div>
+              <div v-if="camera.formatOneTwenty">Format: 120mm</div>
+            </div>
+        </div>
       </div>
     </div>
   </div>
@@ -30,8 +46,32 @@
 <script setup lang="ts">
 import type {Ref} from "vue"
 import {onMounted, ref} from "vue";
-import FriendlyModal from "@/components/FriendlyModal.vue";
-import NewCameraForm from "@/components/NewCameraForm.vue";
+import NewCameraModal from "@/components/NewCameraModal.vue";
+import type {Cameramodel} from "@/types";
+import axios, {type AxiosResponse} from "axios";
+
+const nameField = ref('')
+const brandField = ref('')
+const modelField = ref('')
+const staticImageUrlField = ref('')
+
+async function save () {
+  const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL
+  const endpoint = baseUrl + '/cameramodel'
+  const data: Cameramodel = {
+    name: nameField.value,
+    brand: brandField.value,
+    model: modelField.value,
+    staticImageUrl: staticImageUrlField.value,
+    formatThirtyFive:  document.getElementById("formatSelection").value === "35",
+    formatOneTwenty: document.getElementById("formatSelection").value === "120"
+  }
+  const response: AxiosResponse = await axios.post(endpoint, data);
+  const responseData: Cameramodel = response.data;
+  console.log(responseData);
+  // now reload the page
+  location.reload();
+}
 
 type Camera = {
   id: number,
