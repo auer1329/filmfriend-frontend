a983a1d0757301a3c735dc5be472fc4f25d1fec9
Théo Thoumine <theo@thoumine.de>
2023-12-27 19:21:54 +0100

    Add roll removal functionality
    Co-authored-by: ZieglerAaron <s0588522@htw-berlin.de>

--- src/components/MyCamerasList.vue
+++ src/components/MyCamerasList.vue
@@ -31,9 +31,9 @@
       <div class="card " style="max-width: 540px">
         <img :src=camera.staticImageUrl class="card-image-top rounded-start" :alt="camera.name" >
         <div class="card-body">
-          <h5 class="card-title">{{ camera.name }}</h5>
+          <h5 class="card-title">{{ camera.name }} ({{ camera.id }})</h5>
           <div class="card-text">
-            {{ camera.brand }} {{ camera.model }}  <br>
+            {{ camera.brand }} {{ camera.model }}<br>
             <div v-if="camera.formatThirtyFive">Format: 35mm</div>
             <div v-if="camera.formatOneTwenty">Format: 120mm</div>
           </div>
@@ -43,13 +43,32 @@
               <img :src=camera.roll.stock.staticImageUrl :alt=camera.roll.stock.name width="50" height="50"><br>
               <b>eingelegter Film:</b><br>
               {{ camera.roll.stock.name }} {{ camera.roll.stock.brand }} (ISO: {{ camera.roll.stock.iso }}) <br>
+              <button type="button" class="btn btn-primary" @click=removeRoll(camera.id)>Film entfernen</button>
             </div>
             <div v-else>
               <br>
               <b>kein Film eingelegt</b><br>
-              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newCameraModal">Film einlegen</button>
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#stockSelectionModal" @click="prepareInsert(camera.id)">Film einlegen</button>
               </div>
           </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<!-- Stock Selection Modal -->
+  <div class="modal fade" id="stockSelectionModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Film einlegen</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <input placeholder="StockID" v-model="selectedStockId">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary"  data-bs-dismiss="modal" @click="insertRoll()">Save changes</button>
         </div>
       </div>
     </div>
@@ -59,7 +78,6 @@
 <script setup lang="ts">
 import type {Ref} from "vue"
 import {onMounted, ref} from "vue";
-import NewCameraModal from "@/components/NewCameraModal.vue";
 import type {Cameramodel} from "@/types";
 import axios, {type AxiosResponse} from "axios";
 
@@ -67,6 +85,9 @@ const nameField = ref('')
 const brandField = ref('')
 const modelField = ref('')
 const staticImageUrlField = ref('')
+
+const selectedCameraId = ref(0)
+const selectedStockId = ref(0)
 
 async function save () {
   const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL
@@ -83,6 +104,28 @@ async function save () {
   const responseData: Cameramodel = response.data;
   console.log(responseData);
   // now reload the page
+  location.reload();
+}
+
+function prepareInsert(cameraId: number) {
+  selectedCameraId.value = cameraId
+}
+
+async function insertRoll() {
+  const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL
+  const endpoint = baseUrl + '/roll?cameraId=' + selectedCameraId.value + '&stockId=' + selectedStockId.value
+  console.log(endpoint)
+  const responseData: Cameramodel = await axios.post(endpoint);
+  console.log('Success: ', responseData);
+  location.reload();
+}
+
+async function removeRoll(cameraId: number) {
+  const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL
+  const endpoint = baseUrl + '/roll/' + cameraId
+  console.log(endpoint)
+  const responseData: Cameramodel = await axios.delete(endpoint);
+  console.log('Success: ', responseData);
   location.reload();
 }
 
