bd2a325ba247cb25f9928aefb41cd4ee46907b34
Théo Thoumine <theo@thoumine.de>
2024-01-06 22:17:21 +0100

    Change button layout in camera overview
    Co-authored-by: ZieglerAaron <s0588522@htw-berlin.de>

--- src/components/MyCamerasList.vue
+++ src/components/MyCamerasList.vue
@@ -1,11 +1,13 @@
 <template>
-  <button type="button" class="btn btn-primary" data-bs-toggle="modal" style="margin-bottom: 0px; float: right" data-bs-target="#newCameraModal">Neue Kamera hinzufügen</button>
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" style="margin-bottom: 0px; float: right"
+          data-bs-target="#newCameraModal">Neue Kamera hinzufügen
+  </button>
   <div class="row row-cols-1 row-cols-md-3 g-4">
-
     <div class="col" v-for="camera in cameras" :key="camera.id">
       <div class="card ">
-        <button type="button" class="btn-close overlap-close" @click="deleteCamera(camera.id)" aria-label="Close"></button>
-        <img :src=camera.staticImageUrl class="card-image-top rounded-start" :alt="camera.name" >
+        <button type="button" class="btn-close overlap-close" @click="deleteCamera(camera.id)"
+                aria-label="Close"></button>
+        <img :src=camera.staticImageUrl class="card-image-top rounded-start" :alt="camera.name">
         <div class="card-body">
           <h5 class="card-title">{{ camera.name }}</h5>
           <div class="card-text">
@@ -13,29 +15,39 @@
             <div v-if="camera.formatThirtyFive">Format: 35mm</div>
             <div v-if="camera.formatOneTwenty">Format: 120mm</div>
           </div>
-          <div>
-
-          </div>
-          <div class="card-text">
-            <div v-if="camera.roll != null">
-              <br>
-              <img :src=camera.roll.stock.staticImageUrl :alt=camera.roll.stock.name width="50" height="50"><br>
-              <b>eingelegter Film:</b><br>
-              {{ camera.roll.stock.name }} {{ camera.roll.stock.brand }} (ISO: {{ camera.roll.stock.iso }}) <br>
-              <button type="button" class="btn btn-primary" style="margin:5px" data-bs-toggle="modal" data-bs-target="#developModal" @click="selectedCameraId = camera.id">Film entwickeln</button>
-              <button type="button" class="btn btn-primary" @click=removeRoll(camera.id)>Film entfernen</button>
-            </div>
-            <div v-else>
-              <br>
-              <b>kein Film eingelegt</b><br>
-              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#stockSelectionModal" @click="selectedCameraId = camera.id; loadStocks()">Film einlegen</button>
+          <div v-if="camera.roll != null">
+            <h5 style="margin-top: 1em">
+              Eingelegter Film
+            </h5>
+            <div class="row g-0" style="margin-bottom: 1em" >
+              <div class="col-md-3">
+                <img :src=camera.roll.stock.staticImageUrl :alt=camera.roll.stock.name width="80" height="80">
               </div>
+              <div class="col">
+                {{ camera.roll.stock.name }} {{ camera.roll.stock.brand }} (ISO: {{ camera.roll.stock.iso }})<br>
+              </div>
+            </div>
+            <div class="d-grid ">
+              <div class="btn-group gap-1" role="group" aria-label="process film">
+                <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal"
+                        data-bs-target="#developModal" @click="selectedCameraId = camera.id">Film entwickeln
+                </button>
+                <button type="button" class="btn btn-outline-primary" @click=removeRoll(camera.id)>Film entfernen</button>
+              </div>
+            </div>
+          </div>
+          <div v-else>
+            <div class="d-grid" style="margin-top: 1em">
+              <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#stockSelectionModal"
+                      @click="selectedCameraId = camera.id; loadStocks()">Film einlegen
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-<!-- Stock Selection Modal -->
+  <!-- Stock Selection Modal -->
   <div class="modal fade" id="stockSelectionModal">
     <div class="modal-dialog">
       <div class="modal-content">
@@ -45,7 +57,8 @@
         </div>
         <div class="modal-body">
           <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" style="margin-bottom: 20px" type="search" v-model="searchField" placeholder="Search" aria-label="Search">
+            <input class="form-control mr-sm-2" style="margin-bottom: 20px" type="search" v-model="searchField"
+                   placeholder="Search" aria-label="Search">
           </form>
           <div class="row row-cols-1 g-4">
             <div class="col" v-for="stock in filterStocks()" :key="stock.id">
@@ -57,20 +70,23 @@
                   <div class="col-md-8">
                     <div class="card-body">
                       <h5 class="card-title">{{ stock.brand }} {{ stock.name }}</h5>
-                      <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="selectedStockId = stock.id; insertRoll()">Diesen Film einlegen</button>
+                      <button type="button" class="btn btn-primary" data-bs-dismiss="modal"
+                              @click="selectedStockId = stock.id; insertRoll()">Diesen Film einlegen
+                      </button>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>        </div>
+          </div>
+        </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Abbrechen</button>
         </div>
       </div>
     </div>
   </div>
-<!-- Develop Modal -->
+  <!-- Develop Modal -->
   <div class="modal fade" id="developModal">
     <div class="modal-dialog">
       <div class="modal-content">
@@ -85,13 +101,14 @@
           <input placeholder="Notes" v-model="notesField">
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary"  data-bs-dismiss="modal" @click="developRoll()">Save changes</button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Abbrechen</button>
+          <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="developRoll()">Save changes
+          </button>
         </div>
       </div>
     </div>
   </div>
-<!-- New Camera Modal -->
+  <!-- New Camera Modal -->
   <div class="modal fade" id="newCameraModal">
     <div class="modal-dialog">
       <div class="modal-content">
@@ -100,25 +117,25 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-            <form>
-              <div>
-                <br>
-                <input type="text" class="form-control" v-model="nameField" placeholder="Kameraname" required>
-                <br>
-                <input type="text" class="form-control" v-model="brandField" placeholder="Marke" required>
-                <br>
-                <input type="text" class="form-control" v-model="modelField" placeholder="Modell" required>
-                <br>
-                <input type="text" class="form-control" v-model="staticImageUrlField" placeholder="Bild">
-                <br>
-                <select class="form-select" id="formatSelection" required>
-                  <option value="1" selected disabled>Format auswählen</option>
-                  <option value="35">35mm</option>
-                  <option value="120">120mm</option>
-                </select>
-                <br>
-              </div>
-            </form>
+          <form>
+            <div>
+              <br>
+              <input type="text" class="form-control" v-model="nameField" placeholder="Kameraname" required>
+              <br>
+              <input type="text" class="form-control" v-model="brandField" placeholder="Marke" required>
+              <br>
+              <input type="text" class="form-control" v-model="modelField" placeholder="Modell" required>
+              <br>
+              <input type="text" class="form-control" v-model="staticImageUrlField" placeholder="Bild">
+              <br>
+              <select class="form-select" id="formatSelection" required>
+                <option value="1" selected disabled>Format auswählen</option>
+                <option value="35">35mm</option>
+                <option value="120">120mm</option>
+              </select>
+              <br>
+            </div>
+          </form>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -151,7 +168,7 @@ const expectedPickupDateField = ref('')
 const notesField = ref('')
 
 
-async function save () {
+async function save() {
   const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL
   const endpoint = baseUrl + '/cameramodel'
   const data: Cameramodel = {
@@ -159,7 +176,7 @@ async function save () {
     brand: brandField.value,
     model: modelField.value,
     staticImageUrl: staticImageUrlField.value,
-    formatThirtyFive:  document.getElementById("formatSelection")?.nodeValue === "35",
+    formatThirtyFive: document.getElementById("formatSelection")?.nodeValue === "35",
     formatOneTwenty: document.getElementById("formatSelection")?.nodeValue === "120",
   }
   const response: AxiosResponse = await axios.post(endpoint, data);
@@ -258,16 +275,15 @@ type Stock = {
 }
 
 
-
 const cameras: Ref<Camera[]> = ref([])
 const stocks: Ref<Stock[]> = ref([])
 const searchField: Ref<string> = ref('')
 
-function loadCameras (){
+function loadCameras() {
   const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL
   const endpoint = baseUrl + '/cameramodel'
   const requestOptions: RequestInit = {
-    method:'GET',
+    method: 'GET',
     redirect: 'follow'
   }
   fetch(endpoint, requestOptions)
@@ -285,7 +301,7 @@ function loadCameras (){
       .catch(error => console.log('error', error))
 }
 
-function loadStocks () {
+function loadStocks() {
   const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL
   const endpoint = baseUrl + '/stocks'
   const requestOptions: RequestInit = {
@@ -294,9 +310,10 @@ function loadStocks () {
   }
   fetch(endpoint, requestOptions)
       .then(response => response.json())
-      .then(result => { result.forEach((stock: Stock) => {
-        stocks.value.push(stock)
-      })
+      .then(result => {
+        result.forEach((stock: Stock) => {
+          stocks.value.push(stock)
+        })
       })
       .catch(error => console.log('error', error))
 }
